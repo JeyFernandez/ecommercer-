5e05ab534d7e93ec5389276f7d0106159852ed4a
Jey <jeicobfer@gmail.com>
2021-11-25 17:13:12 -0600

    Agrege la carpeta store y productos dinamicos

--- ecommerce/ecommerce/views.py
+++ ecommerce/ecommerce/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render 
+from store.models import Product 
 #funcion render sirve para mostrar mi vista
 def home(reguest):
-    return render(reguest, 'home.html')
+    products = Product.objects.all().filter(is_available= True)
+
+    context= {
+        'products': products,
+    }
+
+
+    return render(reguest, 'home.html', context)
--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from .models import Category
+from category.models import Category
 # Create your models here.
 
-class Product(models.Models):
-    product_name = models.CharField(max_lengh=100, unique=True)
-    description = models.TextField(max_lengh=255, unique=True)
-    slug = models.CharField(max_lengh=100, unique = True)
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(max_length=255, unique=True)
+    slug = models.CharField(max_length=100, unique = True)
     price = models.IntegerField()
-    images = models.IntegerField(upload_to='photos/products')
+    images = models.ImageField(upload_to='photos/products')
     stock = models.IntegerField()
     is_available= models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
